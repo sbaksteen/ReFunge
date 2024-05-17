5930433b0c9a9710f7cb09e22f1d9c9ac57a2b71
sbaksteen <sdbaksteen@gmail.com>
2024-05-17 22:32:33 +0200

    recordified FungeInts/Strings

--- ReFunge/Data/Values/FungeInt.cs
+++ ReFunge/Data/Values/FungeInt.cs
@@ -1,17 +1,16 @@
 ﻿namespace ReFunge.Data.Values
 {
-    internal readonly struct FungeInt(int value) : IFungeValue<FungeInt>
+    internal readonly record struct FungeInt(int Value) : IFungeValue<FungeInt>
     {
-        private readonly int _value = value;
 
         public static implicit operator FungeInt(int value) => new(value);
 
-        public static implicit operator int(FungeInt me) => me._value;
+        public static implicit operator int(FungeInt me) => me.Value;
 
         public static FungeInt PopFromStack(FungeIP ip) => ip.PopFromStack();
 
         public void PushToStack(FungeIP ip) => ip.PushToStack(this);
 
-        public override string ToString() => _value.ToString();
+        public override string ToString() => Value.ToString();
     }
 }
--- ReFunge/Data/Values/FungeString.cs
+++ ReFunge/Data/Values/FungeString.cs
@@ -2,22 +2,21 @@
 
 namespace ReFunge.Data.Values
 {
-    internal readonly struct FungeString(string value) : IFungeValue<FungeString>, IEnumerable<char>
+    internal readonly record struct FungeString(string Value) : IFungeValue<FungeString>, IEnumerable<char>
     {
-        private readonly string _value = value;
 
         public int Handprint => this.Aggregate(0, (current, c) => current * 256 + c);
 
         public static implicit operator FungeString(string value) => new(value);
 
-        public static implicit operator string(FungeString me) => me._value;
+        public static implicit operator string(FungeString me) => me.Value;
 
         public static FungeString PopFromStack(FungeIP ip) => ip.PopStringFromStack();
 
         public void PushToStack(FungeIP ip) => ip.PushStringToStack(this);
 
-        public IEnumerator<char> GetEnumerator() => ((IEnumerable<char>)_value).GetEnumerator();
+        public IEnumerator<char> GetEnumerator() => ((IEnumerable<char>)Value).GetEnumerator();
 
-        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_value).GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)Value).GetEnumerator();
     }
 }
